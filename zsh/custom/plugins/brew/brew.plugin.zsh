bb2e3295a23a2773c5bb7a1eb5e5519aeec97744
Derek White <derek.white@ul.com>
2019-08-02 09:05:06 -0500

    ZSH: Add enabling homebrew findutils

--- zsh/custom/plugins/brew/brew.plugin.zsh
+++ zsh/custom/plugins/brew/brew.plugin.zsh
@@ -5,3 +5,7 @@ fi
 if [ -d "/usr/local/opt/curl/bin" ] ; then
     export PATH="/usr/local/opt/curl/bin:$PATH"
 fi
+
+if [ -d "/usr/local/opt/findutils/libexec/gnubin" ] ; then
+    export PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
+fi
