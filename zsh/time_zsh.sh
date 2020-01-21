5028b7ba248d9c70077b36c7e996f91a7cb07f79
Derek White <derek.white@ul.com>
2020-01-21 17:09:50 -0600

    ZSH: Change startup timer script to do both login and interactive

--- zsh/time_zsh.sh
+++ zsh/time_zsh.sh
@@ -2,4 +2,8 @@
 # Opens zshell 10 times giving time to load each time
 
 export TIMEFMT='%U user %S system %P cpu %*E total'
+echo "login shell:"
 for i ({1..10}) time zsh -ilc echo &>/dev/null || true
+
+echo "interactive shell:"
+for i ({1..10}) time zsh -ic echo &>/dev/null || true
