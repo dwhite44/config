3700a4ebcfc7c3d7573a8450e88416069f7029a4
Derek White <derek.white@ul.com>
2020-01-21 16:56:30 -0600

    ZSH: Add time_zsh script file to time startup of zsh

---
+++ zsh/time_zsh.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+# Opens zshell 10 times giving time to load each time
+
+export TIMEFMT='%U user %S system %P cpu %*E total'
+for i ({1..10}) time zsh -ilc echo &>/dev/null || true
