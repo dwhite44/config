7651a7eabde7ab22e6d8c5bc40bc65bf2b062ea5
Derek White <derek.white@ul.com>
2020-01-21 16:47:25 -0600

    emacs: macos and linux: remove unnecessary use-package for exec-path-from-shell

--- emacs/core/linux.el
+++ emacs/core/linux.el
@@ -1,8 +1,4 @@
 ;; Linux-specific settings
 
-(use-package exec-path-from-shell
-  :ensure t
-  :defer t)
-
 (require 'exec-path-from-shell)
 (exec-path-from-shell-initialize)
--- emacs/core/macos.el
+++ emacs/core/macos.el
@@ -1,11 +1,5 @@
 ;; Mac-OS specific settings
 
-;; set env when launched as gui app
-(use-package exec-path-from-shell
-  :ensure t
-  :defer t
-  :init)
-
 (require 'exec-path-from-shell)
 ;;(setq exec-path-from-shell-arguments nil)
 (setq exec-path-from-shell-check-startup-files nil)
