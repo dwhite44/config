6abb939487485b80f7d4c2e5335540025ce4db0b
Derek White <derek.white@ul.com>
2021-02-19 19:03:29 -0600

    Emacs Upgrades Feb 2021

--- emacs/elpa/clojure-mode-extra-font-locking-20200320.823/clojure-mode-extra-font-locking-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "clojure-mode-extra-font-locking" "20200320.823" "Extra font-locking for Clojure mode" '((clojure-mode "3.0")) :commit "d69e7cb2eb7052364a455ed7f4136ec68a9b54b6" :keywords '("languages" "lisp") :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "http://github.com/clojure-emacs/clojure-mode")
---
+++ emacs/elpa/parinfer-20201021.254/parinfer-pkg.el
@@ -0,0 +1,13 @@
+(define-package "parinfer" "20201021.254" "Simpler Lisp editing"
+  '((dash "2.13.0")
+    (cl-lib "0.5"))
+  :commit "8659c99a9475ee34af683fdf8f272728c6bebb3a" :authors
+  '(("Shi Tianshu"))
+  :maintainer
+  '("Shi Tianshu")
+  :keywords
+  '("parinfer")
+  :url "https://github.com/DogLooksGood/parinfer-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
