078b4a4372ff0703d6a626ceac8c791634e227e4
Derek White <derek.white@ul.com>
2021-10-13 09:14:00 -0500

    Emacs: Set shitf-<tab> to put in indent/unindent mode

--- emacs/core/editor.el
+++ emacs/core/editor.el
@@ -46,3 +46,8 @@
 ;; Set to allow deleting current text and pasting from clipboard. Prevents
 ;; the deleted text from being put into clipboard")
 (setq save-interprogram-paste-before-kill t)
+
+;; Set Shift-<tab> to put in indent / unindent mode
+(global-set-key (kbd "<backtab>") 'indent-rigidly)
+
+;; End editor.el
