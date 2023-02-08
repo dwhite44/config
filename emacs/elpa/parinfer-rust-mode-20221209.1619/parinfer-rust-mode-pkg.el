3e5292e59d1255984f95b2f7da43499eb61253a9
Derek White <derek.white@ul.com>
2023-02-08 11:45:20 -0700

    Emacs: Upgrade packages

---
+++ emacs/elpa/parinfer-rust-mode-20221209.1619/parinfer-rust-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "parinfer-rust-mode" "20221209.1619" "An interface for the parinfer-rust library"
+  '((emacs "26.1"))
+  :commit "398f3dc4d17c277a4bf9b883cc534f13f2611828" :authors
+  '(("Justin Barclay" . "justinbarclay@gmail.com"))
+  :maintainer
+  '("Justin Barclay" . "justinbarclay@gmail.com")
+  :keywords
+  '("lisp" "tools")
+  :url "https://github.com/justinbarclay/parinfer-rust-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
