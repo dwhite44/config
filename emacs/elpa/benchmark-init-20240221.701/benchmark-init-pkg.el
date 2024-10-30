42fd8dceb9415b54c80aa292e3dd12b6ea649d17
Derek White <derek.white@ul.com>
2024-10-30 08:51:32 -0500

    Emacs package updates

---
+++ emacs/elpa/benchmark-init-20240221.701/benchmark-init-pkg.el
@@ -0,0 +1,14 @@
+(define-package "benchmark-init" "20240221.701" "Benchmarks for require and load calls"
+  '((emacs "24.3"))
+  :commit "80ba80eb62c9449b3920164d999d41b1dd7230eb" :authors
+  '(("Steve Purcell"))
+  :maintainers
+  '(("David Holm" . "dholmster@gmail.com"))
+  :maintainer
+  '("David Holm" . "dholmster@gmail.com")
+  :keywords
+  '("convenience" "benchmark")
+  :url "https://github.com/dholm/benchmark-init-el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs/elpa/lsp-mode-20240225.1747/lsp-mode-pkg.el
@@ -0,0 +1,21 @@
+(define-package "lsp-mode" "20240225.1747" "LSP mode"
+  '((emacs "27.1")
+    (dash "2.18.0")
+    (f "0.20.0")
+    (ht "2.3")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0")
+    (eldoc "1.11"))
+  :commit "dbf13a329c022a9cf33f4d9a91aebb21b0f43adf" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainers
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
